8ca8acde3c37eca1f9034cd6e2d8110af2070173
Israel P. da Silva <israel4p@gmail.com>
2022-04-25 16:11:58 -0300

    Primeiro Dia

---
+++ beerlog/models.py
@@ -0,0 +1,34 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+from sqlmodel import select
+from statistics import mean
+from pydantic import validator
+from datetime import datetime
+
+
+class Beer(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, default=None, index=True)
+    name: str
+    style: str
+    flavor: int
+    image: int
+    cost: int
+    rate: int = 0
+    date: datetime = Field(default_factory=datetime.now)
+
+    @validator("flavor", "image", "cost")
+    def validade_ratings(cls, v, field):
+        if v < 1 or v > 10:
+            raise RuntimeError(f"{field.name}, must be between 1 and 10")
+        return v
+
+    @validator("rate", always=True)
+    def calculate_rate(cls, v, values):
+        rate = mean([values["flavor"], values["image"], values["cost"]])
+        return int(rate)
+
+
+try:
+    brewdog = Beer(name="Bewdog", style="NEIPA", flavor=6, image=8, cost=8)
+except RuntimeError:
+    print("Zika de mais")
